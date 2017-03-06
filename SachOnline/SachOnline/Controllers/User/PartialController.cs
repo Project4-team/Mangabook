d419c9e3d7bd7c507931ac7dc2dd6800d34b4046
Nguyễn Trung Nghĩa <ze@edu.ac.vn>
2017-03-06 23:12:25 +0700

    12

--- SachOnline/SachOnline/Controllers/User/PartialController.cs
+++ SachOnline/SachOnline/Controllers/User/PartialController.cs
@@ -27,5 +27,28 @@ namespace SachOnline.Controllers.User
             var ListSach = db.Saches.ToList();
             return PartialView(ListSach);
         }
+        public ViewResult ChiTietSachView(int masach=0) {
+            Sach sach = db.Saches.SingleOrDefault(n => n.MaSach == masach);
+            if (sach==null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(sach);
+        }
+        public ViewResult SachTheoTheloai(int machude) {
+            ChuDe cd = db.ChuDes.SingleOrDefault(n => n.MaChuDe == machude);
+            if (cd == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            List<Sach> SachChuDe = db.Saches.Where(n => n.MaChuDe == machude).ToList();
+            if (SachChuDe.Count ==0)
+            {
+                ViewBag.Sach = "Không có sách nào thuộc chủ đề này";
+            }
+            return View(SachChuDe);
+        }
     }
 }
